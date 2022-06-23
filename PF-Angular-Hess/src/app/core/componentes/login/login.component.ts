e256f37f844c675c494ea593929d6f052e799edc
Lucas Axel Hess <lucashess98@gmail.com>
2022-06-23 00:43:02 -0300

    sign up en login

--- PF-Angular-Hess/src/app/core/componentes/login/login.component.ts
+++ PF-Angular-Hess/src/app/core/componentes/login/login.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { LoginService } from '../../servicios/login.service';
+import { MatDialog } from '@angular/material/dialog';
+import { UsuarioService } from '../../servicios/usuario.service';
+import { RolesService } from '../../servicios/roles.service';
+import { AbmUsuarioComponent } from 'src/app/features/usuarios/componentes/abm-usuario/abm-usuario.component'; 
+import { MatTable } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { Usuario } from '../../clases/usuario';
+
 
 @Component({
   selector: 'app-login',
@@ -10,7 +18,14 @@ import { LoginService } from '../../servicios/login.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private loginService: LoginService) { }
+@ViewChild(MatTable, {static: true}) table!: MatTable<any>;
+@ViewChild(MatPaginator) paginator!: MatPaginator;
+ds:any;
+
+  constructor(private loginService: LoginService,
+    public dialog:MatDialog, 
+    private servicioUsuario:UsuarioService, 
+    private servicioRoles:RolesService, ) { }
 
   frm:FormGroup=new FormGroup({
     usuario:new FormControl('',[Validators.required]),
@@ -24,5 +39,39 @@ export class LoginComponent implements OnInit {
   ingresar()
   {
     this.loginService.doLogin(this.frm.value.usuario,this.frm.value.clave)
+  }
+
+  altaUsuario()
+  {
+    const refDialog=this.dialog.open(AbmUsuarioComponent,{data:{datosUsr: new Usuario(0,"","",new Date(),0,"",0,"","",1,undefined),
+                                                          rolesPermitidos:this.servicioRoles.getRoles(),
+                                                          soloLectura:false}});
+
+    refDialog.afterClosed().subscribe(result => {
+      if(result!=null)
+      {
+        let addUsr=this.servicioUsuario.addUsuario(result);
+
+        if (addUsr!=null) {
+          addUsr.subscribe(
+            data => {
+
+              this.table.renderRows();
+
+              //this.listar();
+              // this.obtenerUsuarios();
+
+              this.ds.paginator = this.paginator; // el que se mata a pajas
+            }
+          )
+        }
+        // this.table.renderRows();
+        // // this.obtenerUsuarios();
+
+        //  this.ds.paginator = this.paginator;
+      }
+      
+    });
+
   }
 }
--- PF-Angular-Hess/src/app/core/servicios/login.service.ts
+++ PF-Angular-Hess/src/app/core/servicios/login.service.ts
@@ -39,8 +39,6 @@ export class LoginService {
     .subscribe((data)=>{
 
       this.validarDatosUsuario(data,usuario,pass);
-      
-      
     });
 
   }
